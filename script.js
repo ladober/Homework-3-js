6de2a818ad38ff91be332e35f5bb851ccb8b03af
ladober <lado.beridze@gmail.com>
2024-02-01 14:20:20 +0400

    done

--- script.js
+++ script.js
@@ -8,23 +8,47 @@ form.addEventListener("submit", function (event) {
   let todoValue = input.value;
   let newTodo = document.createElement("div");
   newTodo.classList.add("new-todo");
-  newTodo.innerHTML = `<div class="done-btn">
+  newTodo.innerHTML = `
   <div class="inner-app-container">
-    <button type="button" data-action="done" class="btn-action">
-      <img src="done.png" alt="done" />
-    </button>
-    <h1 id="js-todo-item">${todoValue}</h1>
-  </div>
-  <div class="new-todo-tools">
-    <button type="button" data-action="edit" class="btn-action">
-      <img src="edit.png" alt="edit" />
-    </button>
-    <button type="button" data-action="delete" class="btn-action">
-      <img src="del.png" alt="delete" />
-    </button>
-  </div>
-</div>
-  `;
+    <div class="done-btn">
+      <button type="button" data-action="done" class="btn-action-done">
+        <img src="done.png" alt="done" />
+      </button>
+      <h1 class="new-todo-h1" id="js-todo-item">${todoValue}</h1>
+    </div>
+
+    <div class="new-todo-tools">
+      <button type="button" data-action="edit" class="btn-action-edit">
+        <img src="edit.png" alt="edit" />
+      </button>
+      <button
+        type="button"
+        data-action="delete"
+        class="btn-action-delete"
+      >
+        <img src="del.png" alt="delete" />
+      </button>
+    </div>
+  </div> `;
+
+  // <div class="inner-app-container">
+  // <div>
+  //   <button type="button" data-action="done" class="btn-action-done">
+  //     <img src="done.png" alt="done" />
+  //   </button>
+  //   <h1 class="new-todo-h1" id="js-todo-item"></h1>
+  //   </div>
+
+  //  <div>
+  //   <button type="button" data-action="edit" class="btn-action-edit">
+  //     <img src="edit.png" alt="edit" />
+  //   </button>
+  //   <button type="button" data-action="delete" class="btn-action-delete">
+  //     <img src="del.png" alt="delete" />
+  //   </button>
+  //   </div>
+  // </div>
+  // `;
 
   appContainer.append(newTodo);
   input.value = "";
@@ -34,8 +58,7 @@ appContainer.addEventListener("click", taskDel);
 
 function taskDel(event) {
   if (event.target.dataset.action === "delete") {
-    let todoDelete = event.target.closest(".done-btn");
-
+    let todoDelete = event.target.closest(".new-todo");
     todoDelete.remove();
   }
 }
@@ -44,8 +67,19 @@ appContainer.addEventListener("click", taskDone);
 
 function taskDone(event) {
   if (event.target.dataset.action === "done") {
-    let todoDone = event.target.closest(".done-btn");
+    let todoDone = event.target.closest(".new-todo");
 
     todoDone.classList.add("done");
+  }
+}
+
+appContainer.addEventListener("click", taskEdit);
+
+function taskEdit(event) {
+  if (event.target.dataset.action === "edit") {
+    let editText = document.querySelector("#js-todo-item");
+    let editValue = prompt("Please Edit task");
+
+    editText.innerHTML = editValue;
   }
 }
